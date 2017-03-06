08592ac2b5281ee7ba5194e462c3dca94ed11de5
谢辉(外包) <xiehui493@C02Q72YSFVH3-1464.local>
2017-03-06 19:47:07 +0800

    π计算

---
+++ yuan.py
@@ -0,0 +1,11 @@
+# coding:utf8
+import random
+n = 10000000
+k = 0
+for i in range(n):
+    x = random.uniform(-1, 1)
+    y = random.uniform(-1, 1)
+    if x**2 + y**2 < 1:
+        k = k + 1
+
+print 4*float(k)/float(n)
